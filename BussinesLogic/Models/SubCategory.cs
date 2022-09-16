e4936b2669fe9c9aed502a01674658860721188d
Roman Riabenko <beatle.gaze@gmail.com>
2022-09-16 20:56:52 +0300

    Add to SubCategory constructor

--- BussinesLogic/Models/SubCategory.cs
+++ BussinesLogic/Models/SubCategory.cs
@@ -8,5 +8,12 @@ namespace BussinesLogic.Models
         public Category? BaseCategory { get; set; }
         public string? Name { get; set; }
         public List<SubCategory>? SubCategories { get; set; }
+
+        public SubCategory(Category? baseCategory, string? name, List<SubCategory>? subCategories)
+        {
+            BaseCategory = baseCategory;
+            Name = name;
+            SubCategories = subCategories;
+        }
     }
 }
