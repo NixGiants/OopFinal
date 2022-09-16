3099df5a405ddd8d853d95b7e697ba275b580470
Roman Riabenko <beatle.gaze@gmail.com>
2022-09-16 20:58:01 +0300

    Add to Review constructor

--- BussinesLogic/Models/Review.cs
+++ BussinesLogic/Models/Review.cs
@@ -3,9 +3,13 @@ namespace BussinesLogic.Models
 {
     public class Review
     {
-        public ApplicationUser UserFrom { get; set; }
-        public string Text { get; set; }
+        public ApplicationUser? UserFrom { get; set; }
+        public string? Text { get; set; }
 
-
+        public Review(ApplicationUser? userFrom, string? text)
+        {
+            UserFrom = userFrom;
+            Text = text;
+        }
     }
 }
