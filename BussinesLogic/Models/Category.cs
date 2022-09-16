1e9d3b3016f3dc897d68f84d99db367beef29e24
Roman Riabenko <beatle.gaze@gmail.com>
2022-09-16 20:02:53 +0300

    Add constructor for Category

--- BussinesLogic/Models/Category.cs
+++ BussinesLogic/Models/Category.cs
@@ -4,6 +4,14 @@ namespace BussinesLogic.Models
 {
     public class Category : ICategory
     {
-        public string? Name { get; set; }
+        public string Name { get; set; }
+
+        public Category(string name)
+        {
+            if(String.IsNullOrEmpty(name))
+                throw new ArgumentException("Category name cant be null or empty");
+
+            Name = name;
+        }
     }
 }
