9b6aa17285b3f97cf835e95366de076e732f59a5
Roman Riabenko <beatle.gaze@gmail.com>
2022-09-16 20:05:56 +0300

    Add constructor to ApplicationUser

--- BussinesLogic/Models/ApplicationUser.cs
+++ BussinesLogic/Models/ApplicationUser.cs
@@ -1,8 +1,13 @@
 ﻿
+using BussinesLogic.Enums;
+using BussinesLogic.Interfaces;
+
 namespace BussinesLogic.Models
 {
     public class ApplicationUser : Credentials
     {
-       
+        public ApplicationUser(string firstName, string lastName, List<IPaymentMethod>? paymentMethods, string? login, string? password, Access access) : base(firstName, lastName, paymentMethods, login, password, access)
+        {
+        }
     }
 }
