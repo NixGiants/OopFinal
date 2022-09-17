b029c2d2b15fa4bfa3c5a5610defe9806a41791f
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-09-17 18:12:14 +0300

    Prepare Testing data for Console interface

--- OopFinal/Program.cs
+++ OopFinal/Program.cs
@@ -4,28 +4,40 @@ using BussinesLogic.Models;
 using BussinesLogic.Models.Payment;
 using BussinesLogic.Enums;
 
-IPaymentMethod payment = new GooglePay();
-List<IPaymentMethod> list = new List<IPaymentMethod>();
-list.Add(payment);
-ApplicationUser User = new("Vasya","Vasin", list, "VasyaVasin", "123456",Access.Customer);
-
-ICategory category = new Category("Milk Products");
-
-ICategory category_Tech = new Category("head set");
-
-Product product1 = new("Moloko", "Romol", "15% Milk", null, 40,category,true);
-
-Product product2 = new Product("S128", "Philips", "Very expensive and good head set", null,1500,category_Tech,true);
-
-List<Product> products = new List<Product>();
-products.Add(product1);
-products.Add(product2);
-
-Basket basket = new(products);
-
-Order order = new(User, basket, payment, null);
-
-Console.WriteLine(order);
+Console.WriteLine("Test Program: ");
 
 
+ICategory milkProducts = new Category("Milk Products");
+ICategory headSets = new Category("Head sets");
 
+Product milk = new("Milk", "Romol", "15% Milk", null, 40, milkProducts, true);
+Product sourCream = new("Sour cream", "Zakarpatiia", "25% Sour Cream", null, 80,milkProducts,true);
+Product butter = new Product("Butter", "Romol", "85% Butter", null, 120, milkProducts, true);
+Product hollandCheese = new("Holland Cheese", "Zakarpatia", "Perfect cheese from cow milk", null, 350, milkProducts,true);
+Product serum = new("Serum", "Zakarpatia", "good serum", null, 30, milkProducts, true);
+
+Product sonyHeadSet = new Product("S128", "Philips", "Very expensive and good head set", null, 1500, headSets, true);
+Product samsungHeadSet = new("Buds Pro 2", "Samsung", "New model with cool ANC", null, 8000, headSets, true);
+Product appleHeadSet = new("Air Pods Pro 3", "Apple", "Our coolest model ever", null, 13000, headSets, true);
+Product marshallHeadSet = new("Minor 6", "Marshall", "Old is best", null, 5000, headSets, true);
+Product xiaomiHeadSet = new("Galaxy s355 Air Buds Pro Minor 3000", "Xiaomi", "Supoer Chinese model better than apple...", null, 3, headSets, true);
+
+
+List<Product> listOfMilkProducts = new List<Product>();
+listOfMilkProducts.Add(milk);
+listOfMilkProducts.Add(sourCream);
+listOfMilkProducts.Add(butter);
+listOfMilkProducts.Add(hollandCheese);
+listOfMilkProducts.Add(serum);
+
+List<Product> listOfHeadSets = new List<Product>();
+listOfHeadSets.Add(sonyHeadSet);
+listOfHeadSets.Add(samsungHeadSet);
+listOfHeadSets.Add(appleHeadSet);
+listOfHeadSets.Add(marshallHeadSet);
+listOfHeadSets.Add(xiaomiHeadSet);
+
+
+IPaymentMethod googlePay = new GooglePay();
+IPaymentMethod privatBank = new PrivatBankPay();
+List<IPaymentMethod> listOfPaymentMethods = new List<IPaymentMethod>();
