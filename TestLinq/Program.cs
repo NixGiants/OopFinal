e77a0fc9c599e049b3d86f5b47a0260d8c703ffd
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-16 13:43:17 +0300

    Final testing version

--- TestLinq/Program.cs
+++ TestLinq/Program.cs
@@ -6,15 +6,30 @@ Console.WriteLine("Hello, World!");
 
 Linq_Panasenko panasenkoTest = new Linq_Panasenko();
 panasenkoTest.PrintAllProducts();
+Console.WriteLine();
+
 Console.WriteLine("Filtration by BRAND:\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 panasenkoTest.FiltrByBrand("Romol");
+Console.WriteLine();
+
 Console.WriteLine("Filtration by NAME:\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 panasenkoTest.FiltrByName("Milk");
-Console.WriteLine("Sort by PRICE ascending:\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-panasenkoTest.SortByPriceAsc();
-Console.WriteLine("Sort by Name descending:\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-panasenkoTest.SortByNameDesc();
+Console.WriteLine();
 
+Console.WriteLine("Sort by PRICE ascending:\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+panasenkoTest.SortByPriceAsc();
+Console.WriteLine();
+
+Console.WriteLine("Sort by Name descending:\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+panasenkoTest.SortByNameDesc();
+Console.WriteLine();
+
+Console.WriteLine("Projection 1\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+panasenkoTest.Projection();
+Console.WriteLine();
+
+Console.WriteLine("Projection 2\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+panasenkoTest.OtherProjection();
 
 
 
