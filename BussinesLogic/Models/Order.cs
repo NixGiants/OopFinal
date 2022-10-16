7dae2ee03d25ebbc9f2e1acff0071d6b42d1f1ff
Roman Riabenko <beatle.gaze@gmail.com>
2022-10-16 23:36:54 +0300

    Change null admission in Order class

--- BussinesLogic/Models/Order.cs
+++ BussinesLogic/Models/Order.cs
@@ -4,12 +4,12 @@ namespace BussinesLogic.Models
 {
     public class Order
     {
-        public ApplicationUser? UserInOrder { get; set; }
-        public Basket? BasketInOrder { get; set; }
+        public ApplicationUser UserInOrder { get; set; }
+        public Basket BasketInOrder { get; set; }
         public IPaymentMethod? PaymentInfo { get; set; }
         public IShipmentMethod? ShipmentInfo{ get; set; }
 
-        public Order(ApplicationUser? userInOrder, Basket? basketInOrder, IPaymentMethod? paymentInfo, IShipmentMethod? shipmentInfo)
+        public Order(ApplicationUser userInOrder, Basket basketInOrder, IPaymentMethod? paymentInfo, IShipmentMethod? shipmentInfo)
         {
             UserInOrder = userInOrder;
             BasketInOrder = basketInOrder;
