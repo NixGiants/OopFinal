c974ab4c312cdb743de4d8c3b1d5739739f3cc86
IhorPanasenko <ihor.panasenko1@nure.ua>
2022-10-16 12:08:57 +0300

    Add initial class and filtration test

--- TestLinq/Program.cs
+++ TestLinq/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BussinesLogic.Interfaces;
+using BussinesLogic.Models;
+using BussinesLogic.Models.Payment;
+
 Console.WriteLine("Hello, World!");
+
+Linq_Panasenko panasenkoTest = new Linq_Panasenko();
+panasenkoTest.PrintAllProducts();
+Console.WriteLine("Filtration byb brand:\t~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+panasenkoTest.FiltrByBrand("Romol");
+
+
+
+
